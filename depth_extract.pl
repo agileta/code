54910d2d952ff85714a76bc499b068a610c106bc
agileta <alex.gileta@gmail.com>
2015-04-22 18:21:43 -0500

    Depth for all files in directory

--- depth_extract.pl
+++ depth_extract.pl
@@ -4,24 +4,32 @@
 use strict;
 #use warnings;
 
+opendir my $dir, "/group/palmer-lab/Rats/SD/GBS/vars_" or die "Cannot open directory: $!";
+@files = grep(/vcf$/,readdir($dir));
+print join(" ",@files);
+closedir $dir;
+
+open OUT, ">depths.txt" or die;
 my %depths = ();
-my @depth;
 
-open IN, "chr18_test_set.vcf" or die;
-open OUT, ">chr18depths.txt" or die;
+foreach(@files){
+  my @depth;
 
-while (<IN>){
-  next if /^\#/;
-  chomp;
-  my @line = split("\t",$_);
-  for(my $i=0; $i<=11; $i++){
-    my @depth = split(":",$line[$i+9]);
-    #push extracted depth to array with i as the key
-    push (@{$depths{$i}}, $depth[2]);
-  } 
+  open IN, "$_" or die;
+
+  while (<IN>){
+    next if /^\#/;
+    chomp;
+    my @line = split("\t",$_);
+    for(my $i=0; $i<=11; $i++){
+      my @depth = split(":",$line[$i+9]);
+      #push extracted depth to array with i as the key
+      push (@{$depths{$i}}, $depth[2]);
+    } 
+  }
+  for my $key (sort {$a<=>$b} keys %depths){
+    print OUT ("Sample_$key,",join(",",@{$depths{$key}}),"\n");
+  }
+  close OUT;
+  close IN;
 }
-for my $key (sort {$a<=>$b} keys %depths){
-  print OUT ("Sample_$key,",join(",",@{$depths{$key}}),"\n");
-}
-close OUT;
-close IN;

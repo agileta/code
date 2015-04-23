42f63380d63b397d9032eb45da9c2d3995750ee6
agileta <alex.gileta@gmail.com>
2015-04-23 13:01:19 -0500

    Run on 96 samples...not quite there

--- depth_extract.pl
+++ depth_extract.pl
@@ -3,33 +3,38 @@
 ####This script takes in a VCF file
 use strict;
 #use warnings;
-
-opendir my $dir, "/group/palmer-lab/Rats/SD/GBS/vars" or die "Cannot open directory: $!";
-@files = grep(/vcf$/,readdir($dir));
+print "At LEAST IT FUCKING STARTED";
+$| = 1;
+opendir my $dir, "/group/palmer-lab/Rats/SD/GBS/vars_" or die "Cannot open directory: $!";
+print "I DID IT!";
+my @files = grep(/vcf$/,readdir($dir));
 print join(" ",@files);
 closedir $dir;
 
 open OUT, ">depths.txt" or die;
 my %depths = ();
+my $key;
 
 foreach(@files){
   my @depth;
 
-  open IN, "$_" or die;
+  open IN, "/group/palmer-lab/Rats/SD/GBS/vars_/$_" or die;
 
   while (<IN>){
     next if /^\#/;
     chomp;
     my @line = split("\t",$_);
-    for(my $i=0; $i<=11; $i++){
+    for(my $i=0; $i<=95; $i++){
       my @depth = split(":",$line[$i+9]);
       #push extracted depth to array with i as the key
       push (@{$depths{$i}}, $depth[2]);
     } 
   }
-  for my $key (sort {$a<=>$b} keys %depths){
-    print OUT ("Sample_$key_$_,",join(",",@{$depths{$key}}),"\n");
-  }
   close IN;
+  print "Made it out of the reading";	
+  for my $key (sort {$a<=>$b} keys %depths){
+    print OUT ("Sample-$key-$_,",join(",",@{$depths{$key}}),"\n");
+  }
+  print "$_ finished printing";
 }
 close OUT;
